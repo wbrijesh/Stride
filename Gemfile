b51ecd8765c99db361403c6e67b43be295adfdf8
brijesh <brijesh@wawdhane.com>
2020-07-08 21:22:15 +0530

    projects and nested tasks scaffold

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'webpacker', '~> 4.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 
+gem 'devise'
+gem 'nested_scaffold'
+
 gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :projects do
+    resources :tasks
+  end
+  devise_for :users
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
