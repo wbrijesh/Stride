494e15c810f46c2d4fe5f7f62d354067fee5ba8e
brijesh <brijesh@wawdhane.com>
2020-07-08 21:52:43 +0530

    status option

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :status, inclusion: { in: ['not-started', 'in-progress', 'complete', 'quit'] }
+
+  STATUS_OPTIONS = [
+    ['Not Started', 'not-started'],
+    ['In Progress', 'in-progress'],
+    ['Complete', 'complete'],
+    ['Quit', 'quit']
+  ]
 end
