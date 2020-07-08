ca8519238e8240ade960bd51989f998ea46f5507
brijesh <brijesh@wawdhane.com>
2020-07-08 23:02:40 +0530

    change homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   end
   devise_for :users
   get 'home/index'
-  root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'projects#index'
 end
